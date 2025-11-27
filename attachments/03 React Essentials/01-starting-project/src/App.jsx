f3b10c205ad4fe21e57bfc82583895fdde794ad8
Hiroki Fujiwara <hafnium49@gmail.com>
2025-11-28 05:22:34 +0900

    feat: Add Core Concepts data and update App structure to utilize new data

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,13 +1,36 @@
+import reactImg from './assets/react-core-concepts.png';
+// import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Essential'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max));
+}
+
+
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} className="react-logo" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescriptions[genRandomInt(reactDescriptions.length)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} className="concept-logo" alt="Stylized atom" />
+      <h3>{title}</h3>
+      <p>
+        {description}
+      </p>
+    </li>
   );
 }
 
@@ -16,6 +39,15 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
