406697eaa1b08bc83d4151fc6998a566e7590ae0
Hiroki Fujiwara <hafnium49@gmail.com>
2025-11-29 19:27:04 +0900

    feat: Implement state management for selected topic and update TabButton to handle selection

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,5 +1,7 @@
 
 // import componentImg from './assets/components.png';
+import { use, useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
@@ -8,6 +10,20 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please select a tab"); // React Hook, second value is a function to update the state
+
+  // let tabContent = "Please select a tab";
+  function handSelect(selectedButton) {
+    // selectedButton -> 'Class Counter', 'Hooks Counter', ...
+    // tabContent = selectedButton;
+    setSelectedTopic(selectedButton);
+    // console.log(selectedButton);
+    // console.log(tabContent);
+    console.log('Selected Topic:', selectedTopic); // Note: This will log the old state due to the asynchronous nature of state updates
+  }
+  console.log('App Rendered');
+  
+
   return (
     <div>
       <header>
@@ -30,15 +46,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>  
-            {/* <TabButton>Class Counter</TabButton>
-            <TabButton>Hooks Counter</TabButton>
-            <TabButton>Props Demo</TabButton>
-            <TabButton>State Demo</TabButton> */}
-            <TabButton label="Class Counter"/>
+            <TabButton onSelect={() => handSelect('Class Counter')}>Class Counter</TabButton>
+            <TabButton onSelect={() => handSelect('Hooks Counter')}>Hooks Counter</TabButton>
+            <TabButton onSelect={() => handSelect('Props Demo')}>Props Demo</TabButton>
+            <TabButton onSelect={() => handSelect('State Demo')}>State Demo</TabButton>
+            {/* <TabButton label="Class Counter"/>
             <TabButton label="Hooks Counter"/>
             <TabButton label="Props Demo"/>
-            <TabButton label="State Demo"/>
+            <TabButton label="State Demo"/> */}
           </menu>
+          {/* Dynamic Content */}
+          {/* {tabContent} */}
+          {selectedTopic}
         </section>
         <h2>Time to get started!</h2>
       </main>
--- attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
+++ attachments/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,18 +1,28 @@
 // export default function TabButton(props) {
 //   return <li><button>{props.children}</button></li>
 // }
-// export default function TabButton({ children }) {
-//   return (
-//     <li>
-//       <button>{children}</button>
-//     </li>
-//   );
-// }
 
-export default function TabButton({ label }) {
+export default function TabButton({ children, onSelect }) {
+  // document.querySelectorAll('button').forEach(btn => {
+  //   btn.classList.remove('active');
+  // });
+  // this.classList.add('active');
+  
+  // function handleClick() {
+  //   console.log('Hello World');
+  // }
+  console.log('TabButton Rendered');
   return (
     <li>
-      <button>{label}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 }
+
+// export default function TabButton({ label }) {
+//   return (
+//     <li>
+//       <button>{label}</button>
+//     </li>
+//   );
+// }
