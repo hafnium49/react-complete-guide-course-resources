eeeb3d9db8333685f2035f81d044ca24e6f8f76a
Hiroki Fujiwara <hafnium49@gmail.com>
2025-12-03 05:38:14 +0900

    feat: add Player component for managing player names and editing functionality

--- attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,32 @@
-
+import Player from "./components/Player.jsx"
 function App() {
   
 
+  // return (
+  //   // <header>
+  //   //   <img src="https://reactjs.org/logo-og.png" alt="React Logo" />
+  //   //   <h1>React Tic-Tac-Toe</h1>
+  //   // </header>
+  //   <p>Commming soon...</p>
+  // )
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>  
+      <div id="game-container">
+        <ol id="players">
+          {/* <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li> */}
+          <Player name="Player 1" symbol="X" /> {/* Same component different instance */}
+          <Player name="Player 2" symbol="O" /> {/* Same component different instance */}
+        </ol>
+        <ol id="game-board">
+        </ol>
+      </div>
+    </main>
   )
 }
 
---
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+export default function Player({ name, symbol }) {
+    const [playerName, setPlayerName] = useState(name);
+    const [isEditing, setIsEditing] = useState(false);
+
+    // ## Exercise Time!
+    // Pause the video and try completing these tasks on your own
+    // After the pause, I'll show you my solution & implementation
+    // ---
+    // ### Exercise Tasks
+    // 1) Add a function that's triggered when the `<button>` is clicked
+    // 2) Change `isEditing` to `true` in that function
+    // 3) Show the `<span className="player-name">` only when `isEditing` is false
+    // 4) Show an `<input>` element (which does not need to work) if `isEditing` is true
+    // ***
+
+    function handleEditClick() {
+        // setIsEditing(isEditing ? false : true);
+        // setIsEditing(!isEditing); // schedules a state update based on the last updated state value
+        setIsEditing((editing) => !editing); // schedules a state update based on the latest available state value
+    }
+
+    function handleChange(event) {
+        // console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = "Edit";
+    if (isEditing) {
+        // playerName = <input type="text" required defaultValue={name} />;
+        // playerName = <input type="text" required Value={name} />;
+        // btnCaption = "Save";
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />; // two-way binding
+    }
+
+    return (
+        <li>
+            <span className="player">
+              {editablePlayerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    )
+}
+          
