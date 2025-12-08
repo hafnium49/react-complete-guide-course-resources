56df9cd460d90ec76a369c8c0b46379ca424143f
Hiroki Fujiwara <hafnium49@gmail.com>
2025-12-09 05:41:00 +0900

    refactor: update components to use Tailwind CSS for styling and remove styled-components

--- attachments/07 Styling/01-starting-project/src/components/Header.jsx
+++ attachments/07 Styling/01-starting-project/src/components/Header.jsx
@@ -43,15 +43,15 @@ import logo from '../assets/logo.png';
 //     }
 // }
 
-// `;
+// `
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-center my-8 md:mb-16">
+    <header className="flex flex-col items-center justify-center my-8 mb-8 md:mb-16">
       <img src={logo} alt="A canvas" className="object-contain mb-8 w-44 h-44" />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-[#9a3412] font-pacifico mb-4">ReactArt</h1>
+      <h1 className="text-xl font-semibold tracking-widest text-center uppercase md:text-4xl font-semibold tracking-widest text-center uppercase text-[#9a3412] font-pacifico mb-4">ReactArt</h1>
       {/* <p style={{ color: 'red', textAlign: 'left' }}>A community of artists and art-lovers.</p> */}
-      {/* <p className={classes.paragraph}>A community of artists and art-lovers.</p> Using CSS Modules You can add conditions as well */}
+      <p className="text-stone-500">A community of artists and art-lovers.</p> {/*Using CSS Modules You can add conditions as well*/}
     </header>
   );
 }
