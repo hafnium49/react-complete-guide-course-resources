0af23c4c8117b9b27195decdf7df3a39cef3e13b
Hiroki Fujiwara <hafnium49@gmail.com>
2025-12-17 05:30:05 +0900

    feat: implement geolocation-based sorting for available places and refactor Modal component to use open prop

--- attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -1,23 +1,82 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
+
+// store local values in initialization code
+const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIDs.map((id) => 
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
 
 function App() {
-  const modal = useRef();
+  // // store local values in initialization code
+  // const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  // const storedPlaces = storedIDs.map((id) => 
+  //   AVAILABLE_PLACES.find((place) => place.id === id)
+  // );
+
+
+  // const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [modelIsOpen, setModelIsOpen] = useState(false);
+  // const [availablePlaces, setAvailablePlaces] = useState(AVAILABLE_PLACES);
+  // const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  // // local storage does not use useEffect because it does not create an infinite loop
+  // useEffect(() => {
+  //   const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  //   const storedPlaces = storedIDs.map((id) => 
+  //     AVAILABLE_PLACES.find((place) => place.id === id)
+  //   );
+  //   // console.log(storedPlaces);
+  //   setPickedPlaces(storedPlaces);
+  // }, []); // empty dependency array to run only once on mount
+  // // redundant usage of useEffect because local storage executes only once on mount
+
+  // runs after the component renders
+  useEffect(() => {
+    // to get user location using a callback function
+    // side effect
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES, 
+        position.coords.latitude, 
+        position.coords.longitude);
+      // console.log(position);
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []); // empty dependency array to run only once on mount
+  // runs only when the dependency array changes
+  // avoids infinite loop
+
+  // to get user location using a callback function
+  // side effect
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   const sortedPlaces = sortPlacesByDistance(
+  //     AVAILABLE_PLACES, 
+  //     position.coords.latitude, 
+  //     position.coords.longitude);
+  //   // console.log(position);
+  //   setAvailablePlaces(sortedPlaces); // causes infinite loop
+  // });
+
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    // modal.current.open();
+    setModelIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    // modal.current.close();
+    setModelIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -28,18 +87,34 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    // local storage, another side effect which does not need useEffect because it will not create an infinite loop
+    const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIDs.indexOf(id)===-1) {
+      localStorage.setItem(
+        'selectedPlaces', 
+        JSON.stringify([id, ...storedIDs])
+      );
+    }
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    // modal.current.close();
+    setModelIsOpen(false);
+    // local storage
+    const storedIDs = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    const updatedIDs = storedIDs.filter((id) => id !== selectedPlace.current);
+    localStorage.setItem(
+      'selectedPlaces', 
+      JSON.stringify(updatedIDs)
+    );
   }
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modelIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -63,7 +138,9 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          // places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by your location..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
--- attachments/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
+++ attachments/11 Side Effects useEffect/01-starting-project/src/components/Modal.jsx
@@ -1,19 +1,38 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+      if (open) {
+    dialog.current.showModal();
+  } else {
+    dialog.current.close();
+  }
+  }, [open]);
+  // populate ref before using using useEffect
+  // useEffect only cares about dependencies, not about when it runs. i.e. it runs only once after first render because dependency array is empty
+
+  // // // backdrop needs to be handled manually if using open attribute
+  // // dialog.current.showModal();
+  // if (open) {
+  //   dialog.current.showModal();
+  // } else {
+  //   dialog.current.close();
+  // }
+  // // error because ref is undefined on first render
+
+  // useImperativeHandle(ref, () => {
+  //   return {
+  //     open: () => {
+  //       dialog.current.showModal();
+  //     },
+  //     close: () => {
+  //       dialog.current.close();
+  //     },
+  //   };
+  // });
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
@@ -21,6 +40,6 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     </dialog>,
     document.getElementById('modal')
   );
-});
+};
 
 export default Modal;
